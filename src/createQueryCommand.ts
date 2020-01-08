19a351f144041a13a33bc4c8414900917f05207f
Bruno Quaresma <bruno_nonato_quaresma@hotmail.com>
2020-01-08 16:39:45 -0300

    Add default query on createQuery command

--- src/createQueryCommand.ts
+++ src/createQueryCommand.ts
@@ -2,7 +2,8 @@ import vscode from 'vscode';
 
 export default () => async () => {
   let doc = await vscode.workspace.openTextDocument({
-    language: 'fql'
+    language: 'fql',
+    content: 'Paginate(Collections())'
   });
 
   await vscode.window.showTextDocument(doc, { preview: true });
