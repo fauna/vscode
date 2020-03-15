8c09b6b5aac80a4cf4b61dcdd511f036fe32fb9d
Nikhil Saraf <nsaraf98@gmail.com>
2020-03-15 17:51:40 -0400

    add support for upload, merge, override graphql schema

---
+++ src/uploadGraphqlSchemaCommand.ts
@@ -0,0 +1,54 @@
+import vscode from 'vscode';
+
+export default (
+  mode: string = 'merge',
+  adminSecretKey: string,
+  outputChannel: vscode.OutputChannel
+) => async () => {
+  const { activeTextEditor } = vscode.window;
+
+  if (!activeTextEditor || activeTextEditor.document.languageId !== 'graphql') {
+    vscode.window.showWarningMessage(
+      'You have to select a Graphql document to upload a schema.'
+    );
+    return;
+  }
+
+
+  const selection = activeTextEditor.selection;
+  const selectedText = activeTextEditor.document.getText(selection);
+  const fqlExpression = selectedText.length > 0 ? selectedText : activeTextEditor.document.getText();
+  if (fqlExpression.length < 1) {
+     vscode.window.showWarningMessage(
+      'Selected file or selected text must have a GraphQL Schema to run'
+    );
+    
+    return;
+  }
+
+  outputChannel.appendLine('');
+  outputChannel.appendLine(`UPLOADING SCHEMA: ${activeTextEditor.document.fileName}`);
+  outputChannel.show();
+
+  try {
+    const buffer = Buffer.from(fqlExpression, "utf-8");
+    const result = await fetch(`https://graphql.fauna.com/import?mode=${mode}`, {
+      
+      method: "POST",
+      headers: {
+        'AUTHORIZATION': `Bearer ${adminSecretKey}`
+      },
+
+      body: buffer
+    });
+    outputChannel.appendLine('');
+    outputChannel.appendLine('RESPONSE:');
+    outputChannel.appendLine(await result.text());
+
+  } catch (error) {
+    let message = error.message;
+
+    outputChannel.appendLine('ERROR:');
+    outputChannel.appendLine(error);
+  }
+};
