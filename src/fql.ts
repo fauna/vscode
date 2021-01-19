ffa1ec1b69f2727dd89cf4d7afa6236807618542
SZinkevych <szinkevych@lohika.com>
2021-01-19 11:32:12 +0200

    update query parser

--- src/fql.ts
+++ src/fql.ts
@@ -221,19 +221,48 @@ function baseEvalFQL(fql: string, q: typeof query) {
   return fql.match(/^\s*{(.*\n*)*}\s*$/) ? eval(`(${fql})`) : eval(fql);
 }
 
-function splitQueries(code: string): string[] {
-  return code
-    .split(new RegExp(/(?:;)/g))
-    .filter((item) => item.trim().length > 0)
-    .map((item) => item.trim());
+
+function parseQueries(code: string): string[] {
+  const brackets: Record<string, string> = {
+      '{': '}',
+      '(': ')',
+      '[': ']',
+      '"': '"',
+      '\'': '\'',
+  }
+  const openBrackets = Object.keys(brackets)
+  const closeBrackets = Object.values(brackets)
+  const queries = [];
+  const stack = [];
+  let start = 0;
+  let isOpening;
+  code = code.trim();
+
+  for (let i = 0; i < code.length; i++) {
+      if(openBrackets.includes(code[i])){
+          stack.push(code[i]);
+          isOpening = true;
+      }
+
+      if(closeBrackets.includes(code[i]) && brackets[stack.pop()!] !== code[i]){
+          throw new Error(`Unexpected closing bracket ${code[i]} at position: ${i + 1}`)
+      }
+
+      if(stack.length === 0 && isOpening) {
+        queries.push(code.slice(start, i + 1));
+          start = i + 1;
+          isOpening = false;
+      }
+  }
+  return queries;
 }
 
 export function runFQLQuery(code: string, client: Client) {
-  if (!code.trim()) {
-    return Promise.reject("Can not eval empty query.");
-  }
   try {
-    const queriesArray = splitQueries(code)
+    const queriesArray = parseQueries(code)
+    if(queriesArray.length ===0){
+      return Promise.reject('Invalid query')
+    }
 
     const wrappedQueries = queriesArray.map(query => {
       return client.query(evalFQLCode(query))
